1e32ec04ddd9a5bf80b9df3469790bc51afbaff1
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-05-16 17:04:08 +0500

    Added migrations for user

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve('.env.development.local') });
+
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT),
+    host: process.env.POSTGRES_HOST,
+  },
+  test: {},
+  production: {},
+};
