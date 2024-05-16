e28ef08f31a41acd23e2a85723a2a8da26cb4b2e
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-05-16 15:02:49 +0500

    Soft deletion

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -4,17 +4,21 @@ import { sequelize } from '../config/sequelize';
 
 export interface UserInstance extends Model<User>, User {}
 
-export const UserModel = sequelize.define<UserInstance>('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+export const UserModel = sequelize.define<UserInstance>(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
   },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-  },
-});
+  { paranoid: true },
+);
