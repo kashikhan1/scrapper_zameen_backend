6dcae7eaea1bdacadbdcf9094b99084e0515ba75
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-06-21 12:07:11 +0500

    Disabled strict trasport security

--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,7 @@ export class App {
     this.app.use(morgan(LOG_FORMAT, { stream }));
     this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
     this.app.use(hpp());
-    this.app.use(helmet({ strictTransportSecurity: { maxAge: 15552000, includeSubDomains: false } }));
+    this.app.use(helmet({ strictTransportSecurity: false }));
     this.app.use(compression());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
