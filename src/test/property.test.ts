ddf0a747a1f324dead53b34bc3adcd1a5a049028
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-06-25 11:39:18 +0500

    Mock property service to not connect to actual db

--- src/test/property.test.ts
+++ src/test/property.test.ts
@@ -1,12 +1,56 @@
 import request from 'supertest';
+import { Container } from 'typedi';
+import { mock, MockProxy } from 'jest-mock-extended';
 import { App } from '@/app';
 import { PropertyRoute } from '@routes/property.route';
+import { PropertyService } from '@/services/property.service';
+import { getPropertyTypes } from '@/utils/helpers';
+
+jest.mock('@/services/property.service');
+jest.mock('@/utils/helpers', () => ({
+  getPropertyTypes: jest.fn(),
+}));
+const propertyServiceMock: MockProxy<PropertyService> = mock<PropertyService>();
 
 describe('Property', () => {
-  const route = new PropertyRoute();
-  const app = new App([route]).getServer();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  let app;
+  beforeAll(() => {
+    Container.set(PropertyService, propertyServiceMock);
+    const route = new PropertyRoute();
+    route.property.property = propertyServiceMock;
+    app = new App([route]).getServer();
+    (getPropertyTypes as jest.Mock).mockResolvedValue(['House', 'Apartment']);
+  });
   describe('GET /property', () => {
     it('should retrieve properties with default pagination and sorting', async () => {
+      propertyServiceMock.findAllProperties.mockResolvedValue({
+        total_count: 2,
+        properties: [
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 95000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '20 Marla',
+          },
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 9000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '5 Marla',
+          },
+        ],
+      });
       const response = await request(app).get('/property');
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('data');
@@ -18,6 +62,31 @@ describe('Property', () => {
     });
 
     it('should retrieve properties with custom pagination', async () => {
+      propertyServiceMock.findAllProperties.mockResolvedValue({
+        total_count: 2,
+        properties: [
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 95000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '20 Marla',
+          },
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 9000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '5 Marla',
+          },
+        ],
+      });
       const response = await request(app).get('/property?page_size=5&page_number=2');
       expect(response.status).toBe(200);
       expect(response.body.data).toBeInstanceOf(Object);
@@ -27,6 +96,10 @@ describe('Property', () => {
     });
 
     it('should retrieve properties sorted by price in descending order', async () => {
+      propertyServiceMock.findAllProperties.mockResolvedValue({
+        properties: [{ price: 300000 }, { price: 200000 }, { price: 100000 }],
+        total_count: 3,
+      });
       const response = await request(app).get('/property?sort_by=price&sort_order=DESC');
       expect(response.status).toBe(200);
       expect(response.body.data).toBeInstanceOf(Object);
@@ -48,6 +121,12 @@ describe('Property', () => {
     });
   });
   describe('GET /property/count', () => {
+    propertyServiceMock.getPropertiesCountMap.mockResolvedValue({
+      'Agricultural Land': 76,
+      Building: 2618,
+      'Commercial Plot': 2768,
+      Factory: 558,
+    });
     it('should retrieve the total count of properties', async () => {
       const response = await request(app).get('/property/count');
       expect(response.status).toBe(200);
@@ -65,6 +144,7 @@ describe('Property', () => {
   });
   describe('GET /property/:id', () => {
     it('should retrieve a property by ID', async () => {
+      propertyServiceMock.findPropertyById.mockResolvedValue([{ id: 1 }]);
       const response = await request(app).get('/property/1');
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('data');
@@ -75,6 +155,7 @@ describe('Property', () => {
     });
 
     it('should return empty array for a non-existent property ID', async () => {
+      propertyServiceMock.findPropertyById.mockResolvedValue([]);
       const response = await request(app).get('/property/9999');
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('data');
@@ -85,6 +166,33 @@ describe('Property', () => {
   });
   describe('GET /property/:city', () => {
     it('should retrieve properties in a specific city with default pagination and sorting', async () => {
+      propertyServiceMock.findAllProperties.mockResolvedValue({
+        total_count: 2,
+        properties: [
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 95000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '20 Marla',
+            location: 'Islamabad',
+          },
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 9000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '5 Marla',
+            location: 'Islamabad',
+          },
+        ],
+      });
       const response = await request(app).get('/property/islamabad');
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('data');
@@ -99,6 +207,33 @@ describe('Property', () => {
     });
 
     it('should retrieve properties in a specific city with custom pagination', async () => {
+      propertyServiceMock.findAllProperties.mockResolvedValue({
+        total_count: 2,
+        properties: [
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 95000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '20 Marla',
+            location: 'Islamabad',
+          },
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 9000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '5 Marla',
+            location: 'Islamabad',
+          },
+        ],
+      });
       const response = await request(app).get('/property/islamabad?page_size=5&page_number=2');
       expect(response.status).toBe(200);
       expect(response.body.data).toBeInstanceOf(Object);
@@ -111,6 +246,10 @@ describe('Property', () => {
     });
 
     it('should retrieve properties in a specific city sorted by price in descending order', async () => {
+      propertyServiceMock.findAllProperties.mockResolvedValue({
+        properties: [{ price: 300000 }, { price: 200000 }, { price: 100000 }],
+        total_count: 3,
+      });
       const response = await request(app).get('/property/islamabad?sort_by=price&sort_order=DESC');
       expect(response.status).toBe(200);
       expect(response.body.data).toBeInstanceOf(Object);
@@ -128,6 +267,33 @@ describe('Property', () => {
   });
   describe('GET /property/search', () => {
     it('should retrieve properties with default pagination and sorting', async () => {
+      propertyServiceMock.searchProperties.mockResolvedValue({
+        total_count: 2,
+        properties: [
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 95000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '20 Marla',
+            location: 'Islamabad',
+          },
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 9000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '5 Marla',
+            location: 'Islamabad',
+          },
+        ],
+      });
       const response = await request(app).get('/property/search');
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('data');
@@ -139,6 +305,33 @@ describe('Property', () => {
     });
 
     it('should retrieve properties with custom pagination', async () => {
+      propertyServiceMock.searchProperties.mockResolvedValue({
+        total_count: 2,
+        properties: [
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 95000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '20 Marla',
+            location: 'Islamabad',
+          },
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 9000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '5 Marla',
+            location: 'Islamabad',
+          },
+        ],
+      });
       const response = await request(app).get('/property/search?page_size=5&page_number=2');
       expect(response.status).toBe(200);
       expect(response.body.data).toBeInstanceOf(Object);
@@ -148,6 +341,10 @@ describe('Property', () => {
     });
 
     it('should retrieve properties sorted by price in descending order', async () => {
+      propertyServiceMock.findAllProperties.mockResolvedValue({
+        properties: [{ price: 300000 }, { price: 200000 }, { price: 100000 }],
+        total_count: 3,
+      });
       const response = await request(app).get('/property/search?sort_by=price&sort_order=DESC');
       expect(response.status).toBe(200);
       expect(response.body.data).toBeInstanceOf(Object);
@@ -167,7 +364,39 @@ describe('Property', () => {
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty('message');
     });
+    it('should return 400 for invalid sorting parameters (sort_order)', async () => {
+      const response = await request(app).get('/property/search?sort_by=price&sort_order=unknown');
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('message');
+    });
     it('should search', async () => {
+      propertyServiceMock.searchProperties.mockResolvedValue({
+        total_count: 2,
+        properties: [
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 95000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '20 Marla',
+            location: 'Islamabad',
+          },
+          {
+            id: 1,
+            desc: 'description',
+            header: 'Header',
+            type: 'House',
+            price: 9000000,
+            cover_photo_url: 'cover_photo_url.jpeg',
+            available: true,
+            area: '5 Marla',
+            location: 'Islamabad',
+          },
+        ],
+      });
       const responses = await Promise.all([
         request(app).get('/property/search?query=islamabad'),
         request(app).get('/property/search?query=123'),
