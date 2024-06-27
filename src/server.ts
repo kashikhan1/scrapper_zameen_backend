a032ff4fb89ae3ff18b0186ed0a91fd192c585de
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-06-27 17:00:39 +0500

    refactor: removed unused code

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import { App } from '@/app';
-import { AuthRoute } from '@routes/auth.route';
-import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
 import { PropertyRoute } from '@routes/property.route';
 
 ValidateEnv();
 
-const app = new App([new UserRoute(), new AuthRoute(), new PropertyRoute()]);
+const app = new App([new PropertyRoute()]);
 
 app.listen();
