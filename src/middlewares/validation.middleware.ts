edfb1e8ea737f5f52de0b4fc7926724bfed3bf5a
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-07-19 19:29:33 +0500

    refactor: typescript issues

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
 import { AVAILABLE_CITIES } from '@/types';
 import { getPropertyPurpose, getPropertyTypes } from '@/utils/helpers';
+import { PropertyPurposeType, PropertyType } from '@/models/models';
 
 /**
  * @name ValidationMiddleware
@@ -96,7 +97,7 @@ export const validateSearchFiltersMiddleware = async (req: Request, res: Respons
       return res.status(400).json({ message: 'Invalid price parameters. Both price_min and price_max must be valid numbers.' });
     case bedrooms && bedrooms.split(',').some(v => isNaN(Number(v))):
       return res.status(400).json({ message: 'Invalid bedrooms parameter. It must be a valid number.' });
-    case property_type && !PROPERTY_TYPES.includes(property_type as string):
+    case property_type && !PROPERTY_TYPES.includes(property_type as PropertyType):
       return res.status(400).json({ message: `Invalid property_type parameter. It must be one of following: ${PROPERTY_TYPES.join(', ')}` });
     case isNaN(Number(area_min)) || Number(area_min) < 0:
     case area_max && (isNaN(Number(area_max)) || Number(area_max) < 0):
@@ -120,7 +121,7 @@ export const validatePropertyId = (req: Request, res: Response, next: NextFuncti
 
 export const validatePurposeFilter = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { purpose = 'for_sale' } = req.query as { purpose: string };
+    const { purpose = 'for_sale' } = req.query as { purpose: PropertyPurposeType };
     const dbPurpose = await getPropertyPurpose();
     if (!dbPurpose.includes(purpose)) {
       return res.status(400).json({ message: `Invalid purpose parameter. It must be one of following: ${dbPurpose.join(',')}.` });
--- src/models/models.ts
+++ src/models/models.ts
@@ -5,35 +5,39 @@ const { TABLE_NAME: CITIES_TABLE } = require('../migrations/20240713080551-creat
 const { TABLE_NAME: LOCATIONS_TABLE } = require('../migrations/20240714103816-locations');
 const { TABLE_NAME: PROPERTIES_TABLE } = require('../migrations/20240715121921-create-properties');
 
+export type PropertyType =
+  | 'agricultural_land'
+  | 'building'
+  | 'commercial_plot'
+  | 'factory'
+  | 'farm_house'
+  | 'flat'
+  | 'house'
+  | 'industrial_land'
+  | 'office'
+  | 'other'
+  | 'penthouse'
+  | 'plot_file'
+  | 'plot_form'
+  | 'residential_plot'
+  | 'room'
+  | 'shop'
+  | 'lower_portion'
+  | 'upper_portion'
+  | 'warehouse';
+
+export type PropertyPurposeType = 'for_sale' | 'for_rent';
+
 interface PropertiesModel extends Model<InferAttributes<PropertiesModel>, InferCreationAttributes<PropertiesModel>> {
   id: CreationOptional<number>;
   desc: string;
   header: string;
-  type:
-    | 'agricultural_land'
-    | 'building'
-    | 'commercial_plot'
-    | 'factory'
-    | 'farm_house'
-    | 'flat'
-    | 'house'
-    | 'industrial_land'
-    | 'office'
-    | 'other'
-    | 'penthouse'
-    | 'plot_file'
-    | 'plot_form'
-    | 'residential_plot'
-    | 'room'
-    | 'shop'
-    | 'lower_portion'
-    | 'upper_portion'
-    | 'warehouse';
+  type: PropertyType;
   price: number;
   location_id: number;
   bath: number;
   area: number;
-  purpose: 'for_sale' | 'for_rent';
+  purpose: PropertyPurposeType;
   bedroom: number;
   added: Date;
   initial_amount: string;
