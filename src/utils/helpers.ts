0ae09ab561a7c433262088924e86ed4376facbbc
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-07-26 11:28:02 +0500

    perf: cache frequently used queries results

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,16 +1,34 @@
+import Container from 'typedi';
 import { sequelize } from '@/config/sequelize';
 import { Property } from '@/models/models';
+import { RedisService } from '@/services/redis.service';
 
 export const getPropertyTypes = async () => {
+  const redis = Container.get(RedisService);
+  const cacheKey = `getPropertyTypes`;
+  const redisValue = await redis.getRedisValue(cacheKey);
+  if (redisValue) {
+    return JSON.parse(redisValue);
+  }
   const result = await Property.findAll({
     attributes: [[sequelize.fn('DISTINCT', sequelize.col('type')), 'type']],
   });
-  return result.map(item => item?.['type']).filter(item => item != null);
+  const propertyTypes = result.map(item => item?.['type']).filter(item => item != null);
+  redis.setRedisValue({ key: cacheKey, value: JSON.stringify(propertyTypes) });
+  return propertyTypes;
 };
 
 export const getPropertyPurpose = async () => {
+  const redis = Container.get(RedisService);
+  const cacheKey = `getPropertyPurpose`;
+  const redisValue = await redis.getRedisValue(cacheKey);
+  if (redisValue) {
+    return JSON.parse(redisValue);
+  }
   const result = await Property.findAll({
     attributes: [[sequelize.fn('DISTINCT', sequelize.col('purpose')), 'purpose']],
   });
-  return result.map(item => item?.['purpose']).filter(item => item != null);
+  const propertyPurpose = result.map(item => item?.['purpose']).filter(item => item != null);
+  redis.setRedisValue({ key: cacheKey, value: JSON.stringify(propertyPurpose) });
+  return propertyPurpose;
 };
