d3de8aa44f19fccd47e23e1db871995f864ddc25
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-08-02 16:08:07 +0500

    refactor: refactored pagination middleware

--- src/middlewares/pagination.middleware.ts
+++ src/middlewares/pagination.middleware.ts
@@ -5,9 +5,10 @@ import { Request, Response, NextFunction } from 'express';
 
 // Middleware to validate page_size and page_number
 export const validatePaginationParamsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  req.query.page_size = req.query.page_size || '10';
-  req.query.page_number = req.query.page_number || '1';
-  const { page_size, page_number } = req.query as unknown as IvalidatePaginationParamsMiddlewareQueryParams;
+  const { query } = req;
+  query.page_size = query.page_size || '10';
+  query.page_number = query.page_number || '1';
+  const { page_size, page_number } = query as unknown as IvalidatePaginationParamsMiddlewareQueryParams;
   if (isInvalidNumber(page_number) || isInvalidNumber(page_size)) {
     return returnBadRequestError({ res, message: 'Invalid pagination parameters. Both page_size and page_number must be valid numbers.' });
   }
@@ -15,9 +16,10 @@ export const validatePaginationParamsMiddleware = (req: Request, res: Response, 
 };
 
 export const validateSortParamsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  req.query.sort_by = req.query.sort_by || SORT_COLUMNS.ID;
-  req.query.sort_order = req.query.sort_order || SORT_ORDER.ASC;
-  const { sort_by, sort_order } = req.query;
+  const { query } = req;
+  query.sort_by = query.sort_by || SORT_COLUMNS.ID;
+  query.sort_order = query.sort_order || SORT_ORDER.ASC;
+  const { sort_by, sort_order } = query;
   if (!Object.values(SORT_COLUMNS).includes(sort_by as SORT_COLUMNS)) {
     return returnBadRequestError({ res, message: 'sort_by must be one of the following: ' + Object.values(SORT_COLUMNS).join(', ') });
   }
