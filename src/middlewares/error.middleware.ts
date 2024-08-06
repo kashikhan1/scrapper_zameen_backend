608d06943a62b25b7e7347ddc6a514c1108353c1
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-08-06 17:07:43 +0500

    feat: send email on internal server error in production env

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
 import { logger } from '@utils/logger';
+import { NODE_ENV, SENDER_EMAIL } from '@/config';
+import { transporter } from '@/config/nodemailer';
+import { generateEmailContent, getSendEmailPayload } from '@/utils';
 
 export const ErrorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
   try {
@@ -14,6 +17,22 @@ export const ErrorMiddleware = (error: HttpException, req: Request, res: Respons
     const status: number = error.status || 500;
 
     logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+
+    if (NODE_ENV === 'production' && status === 500) {
+      const html = generateEmailContent({
+        path: req.path,
+        method: req.method,
+        status: status,
+        message: message,
+      });
+      transporter.sendMail(
+        getSendEmailPayload({
+          to: SENDER_EMAIL,
+          subject: 'Internal Server Error',
+          html,
+        }),
+      );
+    }
     res.status(status).json({ message });
   } catch (error) {
     next(error);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,30 @@
+import Mail from 'nodemailer/lib/mailer';
+import { SENDER_EMAIL } from '@/config';
+
 export const splitAndTrimString = (str: string = '', sep: string = ',') => {
   return str.split(sep).map(s => s.trim());
+};
+
+export const getSendEmailPayload = ({ to, subject, html }: Mail.Options): Mail.Options => {
+  return {
+    from: SENDER_EMAIL,
+    to,
+    subject,
+    html,
+  };
+};
+
+export const generateEmailContent = ({ path, method, status, message }) => {
+  return `
+    <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+      <h2 style="color: #d9534f;">Internal Server Error</h2>
+      <p>The <strong>'scrapper_zameen_backend'</strong> project encountered an internal server error. Details are below:</p>
+      <ul style="list-style-type: none; padding: 0;">
+        <li><strong>Request Path:</strong> ${path}</li>
+        <li><strong>Request Method:</strong> ${method}</li>
+        <li><strong>Response Status:</strong> ${status}</li>
+        <li><strong>Error Message:</strong> ${message}</li>
+      </ul>
+    </div>
+  `;
 };
