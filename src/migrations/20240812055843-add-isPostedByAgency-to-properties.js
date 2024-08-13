d2f6e73f0dac9fb8ab95b01128d32aeeacd7a5d9
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2024-08-13 12:52:16 +0500

    feat: added migrations for agency data

--- src/migrations/20240812055843-add-isPostedByAgency-to-properties.js
+++ src/migrations/20240812055843-add-isPostedByAgency-to-properties.js
@@ -1,9 +1,10 @@
 'use strict';
+const { TABLE_NAME: PROPERTIES_TABLE } = require('./20240715121921-create-properties');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('properties', 'is_posted_by_agency', {
+    await queryInterface.addColumn(PROPERTIES_TABLE, 'is_posted_by_agency', {
       type: Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false,
@@ -11,6 +12,6 @@ module.exports = {
   },
 
   async down(queryInterface, _Sequelize) {
-    await queryInterface.removeColumn('properties', 'is_posted_by_agency');
+    await queryInterface.removeColumn(PROPERTIES_TABLE, 'is_posted_by_agency');
   },
 };
