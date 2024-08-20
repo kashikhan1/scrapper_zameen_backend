1bea97419b45727b70e2dcc17b727a91c0ef1378
Muhammad Kashif Khan <m.kashi.khan93@gmail.com>
2024-08-20 19:34:35 +0500

    Update property.controller.ts

--- src/controllers/property.controller.ts
+++ src/controllers/property.controller.ts
@@ -16,7 +16,7 @@ import { PropertyPurposeType, PropertyType } from '@/models/models';
 export class PropertyController {
   public property = Container.get(PropertyService);
 
-  public getProperties = async (req: IRequestWithSortingParams, res: Response, next: NextFunction): Promise<void> => {
+  public getProperties = async (req: any, res: any, next: any): Promise<void> => {
     try {
       const { query, params, order } = req;
       const { page_size, page_number, purpose } = query as unknown as IGetPropertiesQueryParams;
